0b15c9d3b0c9461ed2069adfa0d7ac6b85ea9681
Anastasios Markos <amarkos@novibet.com>
2022-04-07 17:19:29 +0300

    Add project files.

---
+++ Globals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace TradingBot
+{
+	public static class Globals
+	{
+
+		//Api Key
+		public static string ApiKey { get; set; }
+
+		//Secret Key
+		public static string SecretKey { get; set; }
+
+		//Set to true to make a test order. 
+		//Console will write trade details of last real trade if test
+		public static bool testCase = false;
+		internal static double percentageChange;
+		internal static double quatityPerTrade;
+		internal static string QuoteCurrency;
+		internal static string BaseCurrency;
+		internal static int TimerInterval;
+	}
+}
---
+++ Run.cs
@@ -0,0 +1,36 @@
+﻿using TradingBot.Models;
+using System;
+using System.Timers;
+
+
+
+namespace TradingBot
+{
+	public class Run
+	{
+		static System.Threading.ManualResetEvent timerFired = new System.Threading.ManualResetEvent(false);
+
+		public static void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+		{
+			Console.WriteLine("Timer Fired.");
+			Algorithm.Bot();
+		}
+
+
+		public static void Main(string[] args)
+		{
+			//Start Bot the first time, before timer
+			Algorithm.Bot();
+
+            //Start Timer
+            Timer timer = new Timer
+            {
+                Interval = Globals.TimerInterval * 60 * 1000 // converts ms to minutes
+            };
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+			timer.Enabled = true;
+
+			timerFired.WaitOne();  //https://stackoverflow.com/questions/34958759/timer-does-not-fire-before-application-ends-in-c-sharp?rq=1
+		}
+	}
+}
